ea8e64dd77426425e583b8cb9a095beebb3ace22
sumeco-coder <swynn@swtaxservice.com>
2025-12-23 22:14:14 -0800

    Add emailCampaignRunnerV2 handler

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,6 +5,7 @@ import { storage } from "./storage/resource";
 import { assignRoleOnConfirm } from "./functions/assignRoleOnConfirm/resource";  // <-- REQUIRED IMPORT
 import { socialPostRunner } from "./functions/socialPostRunner/resource";
 import { emailCampaignRunner } from "./functions/emailCampaignRunner/resource";
+import { emailCampaignRunnerV2 } from "./functions/emailCampaignRunnerV2/resource";
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
@@ -15,5 +16,6 @@ defineBackend({
   assignRoleOnConfirm,
   socialPostRunner,
   emailCampaignRunner,
+  emailCampaignRunnerV2,
 });
 
